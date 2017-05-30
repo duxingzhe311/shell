ad9979bc0742c627938a3931304801a6732f9ef9
chenxubing <chenxubing@chicootec.com>
2017-05-30 14:40:23 +0800

    add wyykt

---
+++ wyykt/awk.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+#top | awk '{print $2"\t"$3}'
+#top | awk '{print $1}'
---
+++ wyykt/find.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+cd ~/svn/shell
+#find . -name "*.txt"
+#find . -name "[a-z]*"
+#find . -perm 755
+#find . -mtime -5
+
+#find . -mtime +3
+
+#find . -name "*.java" | xargs rm -rf
